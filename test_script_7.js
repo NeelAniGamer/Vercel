777454f0f26dcba8a46a4d2826d1c2020f384068
NeelAniGamer <subscriptionfound@gmail.com>
2026-06-24 06:56:54 +0530

    Upadte

--- test_script_7.js
+++
@@ -1,9 +0,0 @@
-
-    const _oldFetch = window.fetch;
-    window.fetch = function() {
-      if (arguments[0] && typeof arguments[0] === 'string' && arguments[0].startsWith('config.json')) {
-        arguments[0] = '../' + arguments[0];
-      }
-      return _oldFetch.apply(this, arguments);
-    };
-  
