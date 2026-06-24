fdbb49666e97adaa07fe1cf916d35c43b0a3061a
NeelAniGamer <subscriptionfound@gmail.com>
2026-06-24 20:33:58 +0530

    Update

--- col-ui.js
+++ col-ui.js
@@ -40,6 +40,18 @@
             if(tsck) tsck.checked = true;
         }
     } catch(e) {}
+
+    // 4. Inject Mobile Bottom Navigation
+    if (!document.getElementById('mobileBottomNav')) {
+        var mbn = document.createElement('div');
+        mbn.id = 'mobileBottomNav';
+        mbn.className = 'mobile-bottom-nav';
+        mbn.innerHTML = '<a href="home.html" class="mbn-item"><svg viewBox="0 0 24 24"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>Home</a>' +
+                        '<a href="about.html" class="mbn-item"><svg viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>About</a>' +
+                        '<a href="school.html" class="mbn-item"><svg viewBox="0 0 24 24"><path d="M22 10v6M2 10l10-5 10 5-10 5z"></path><path d="M6 12v5c3 3 9 3 12 0v-5"></path></svg>Academy</a>' +
+                        '<a href="#" class="mbn-item" onclick="var ck=document.getElementById(\\'tsck\\');if(ck){ck.checked=!ck.checked;window.toggleTheme(ck);}else{var isLm=document.body.classList.toggle(\\'lm\\');try{localStorage.setItem(\\'theme\\', isLm?\\'light\\':\\'dark\\');}catch(e){}}return false;"><svg viewBox="0 0 24 24"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>Theme</a>';
+        document.body.appendChild(mbn);
+    }
 })();
 
 // Theme Toggle Function (Global)
