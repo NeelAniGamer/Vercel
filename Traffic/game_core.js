063fd07ca3fd7a04ca101c4534ee74e7d7c5e3af
NeelAniGamer <subscriptionfound@gmail.com>
2026-07-23 19:11:33 +0530

    fixes

--- Traffic/game_core.js
+++ Traffic/game_core.js
@@ -3765,7 +3765,7 @@ class Game {
         
         // ─── WORLD STREAMING + FLOATING ORIGIN ───
         // this._updateStreaming(); // Function undefined
-        this._checkFloatingOrigin();
+        // this._checkFloatingOrigin(); // Function undefined
         
         this._tickEnterExit(dt); this._input(dt); this._usigs(dt); this._unpcs(dt); this._upeds(dt); this._ucps(dt); this._updateArrows(); this._ugps(); this._checkBrakeZones(dt); this._uobs(dt); this._umode(dt); this._updateLights(dt); this._decayCameraLook(dt); this._ucam(dt); this._usun(dt); this._updateDayNight(dt); this._uhud(); this._ummap(); this._utransit(); this._computeTaskFlags(); this._checkTasks(); this._updateRain(dt); this._updateRainAudio(this.mode === 'rain' || this.mapCfg?.hasRain); this._updateDynamicLOD(lodMult); this._updateBreadcrumbPath(dt);
 
